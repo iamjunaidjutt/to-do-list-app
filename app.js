b1b56a0f4c3db536d173ad00b2d94019d28aae37
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-19 17:39:07 +0500

    edit list.html to list.ejs or add currentday into app.js

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { response } = require("express");
 
 const app = express();
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-  let date = new Date();
-  if(date.getDay() === 2) {
-    res.sendFile(__dirname + "/index.html");
-  } else {
-    res.send("Its a weedend!");  
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
+  switch(currentDay) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    default:
+        console.log("Invalid date");
   }
+  res.render("list", {weekday: day});
 });
 
 app.listen(3000, function(){
