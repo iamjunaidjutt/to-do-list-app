8d763bbeed79faada90558dc21fd84b348632b76
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-20 11:43:44 +0500

    Setup the functionality to add new items in to-do-list

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+let items = [];
 app.get("/", function(req, res){
   let today = new Date();
   let options = {
@@ -12,34 +14,14 @@ app.get("/", function(req, res){
     month: "long",
   }
   let formatDate = today.toLocaleDateString("en-US", options);
-  let currentDay = today.getDay();
-  let day = "";
-  switch(currentDay) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-        break;
-    default:
-        console.log("Invalid date");
-  }
-  res.render("list", {weekday: formatDate});
+  
+  res.render("list", {weekday: formatDate, newListItems: items});
+});
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
