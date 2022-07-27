d8048c06dc815bb691d9dad79167a632cf7c40f3
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-28 00:45:43 +0500

    Add mongodb Atlas link and edit list.ejs to add title

--- app.js
+++ app.js
@@ -10,22 +10,30 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/toDoListDB");
+const urlDB = "mongodb+srv://admin-junaid:KZ0yj8yXxmmY81bi@cluster0.afaucgl.mongodb.net/toDoListDB?retryWrites=true&w=majority";
 
+mongoose.connect(urlDB, {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+ }).then(() => {
+	console.log("Connect to database");
+ }).catch(err => {
+	console.log(err.message);
+ });
+ 
 const itemsSchema = mongoose.Schema({
-	name: String
+	name: String,
 });
+
 const Item = mongoose.model("Item", itemsSchema);
 const listSchema = mongoose.Schema({
 	name: String,
-	items: [itemsSchema]
+	items: [itemsSchema],
 });
 const List = mongoose.model("List", listSchema);
 
-
 app.get("/", function (req, res) {
 	Item.find({}, (err, foundItems) => {
-
 		if (err) {
 			console.error(err);
 		} else {
@@ -34,7 +42,6 @@ app.get("/", function (req, res) {
 				newListItems: foundItems,
 			});
 		}
-
 	});
 });
 
@@ -43,23 +50,18 @@ app.get("/:customList", function (req, res) {
 
 	List.findOne({ name: customListName }, (err, foundList) => {
 		if (!err) {
-
 			if (foundList) {
-
 				res.render("list", {
 					listTitle: foundList.name,
 					newListItems: foundList.items,
 				});
 			} else {
-
 				List.create({
 					name: customListName,
 				});
 				res.redirect(`/${customListName}`);
-      }
-
+			}
 		}
-
 	});
 });
 
@@ -83,20 +85,22 @@ app.post("/", function (req, res) {
 
 app.post("/delete", function (req, res) {
 	const itemId = req.body.checkbox;
-  const listName = req.body.listName;
+	const listName = req.body.listName;
 
-  if(listName === "Today") {
-    Item.findByIdAndRemove(itemId, function (err, item) {
-      res.redirect("/");
-    });
-  } else {
-	List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: itemId}}}, function(err, list) {
-		res.redirect(`/${listName}`);
-	});
-  }
-	
+	if (listName === "Today") {
+		Item.findByIdAndRemove(itemId, function (err, item) {
+			res.redirect("/");
+		});
+	} else {
+		List.findOneAndUpdate(
+			{ name: listName },
+			{ $pull: { items: { _id: itemId } } },
+			function (err, list) {
+				res.redirect(`/${listName}`);
+			}
+		);
+	}
 });
-
 
 app.listen(3000, function () {
 	console.log("Server started on port 3000");
